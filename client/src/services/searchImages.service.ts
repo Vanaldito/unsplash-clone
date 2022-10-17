4db708ba3c8cea6e8ec72244d30597e610a267aa
Vanaldito <fredimanuelb@gmail.com>
2022-10-17 10:06:22 -0600

    fix: change name of SearchImages to searchImages

--- client/src/services/searchImages.service.ts
+++ client/src/services/searchImages.service.ts
@@ -10,7 +10,7 @@ interface Ok {
   results: ImageInfo[];
 }
 
-export default async function SearchImages(query: string): Promise<Error | Ok> {
+export default async function searchImages(query: string): Promise<Error | Ok> {
   const res = await fetch(`/api/v1/search?q=${query}`);
   return await res.json();
 }
