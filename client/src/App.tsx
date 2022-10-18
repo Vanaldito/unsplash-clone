e319118a0b32f3ec8f7c16ea36e54765e8c14f91
Vanaldito <fredimanuelb@gmail.com>
2022-10-17 20:33:55 -0600

    feat: use code splitting to separate the pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import { Home, Search } from "./pages";
+import { Loader } from "./components";
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Search = lazy(() => import("./pages/Search/Search"));
 
 import "./App.css";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search" element={<Search />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Suspense>
   );
 }
